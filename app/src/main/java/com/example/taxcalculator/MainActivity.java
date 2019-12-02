e44718fea33196126f0d5b6e74f7d9e798c4569b
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 14:48:40 -0500

    initial

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -2,9 +2,37 @@ package com.example.taxcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class MainActivity extends AppCompatActivity {
+        private Button btnCalculate;
+        private EditText edSIN;
+        private EditText fnm;
+        private EditText lnm;
+        private EditText mainAge;
+        private RadioGroup rgGender;
+        private RadioButton rbMale;
+        private RadioButton rbFemale;
+        private RadioButton rbOthers;
+        String radio = "";
+        private TextView txtDateOfBirth;
+        DatePickerDialog datePickerDialog;
+        DatePickerDialog taxDatePickerDialog;
+        final Calendar calendar = Calendar.getInstance();
+        final Calendar calendarTax = Calendar.getInstance();
+        private TextView taxFilling;
+        private TextView rrspMain;
+        private  EditText grossIncome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
