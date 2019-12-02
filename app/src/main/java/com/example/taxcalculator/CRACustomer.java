559aea2464d5249bd3ee6c42eeb4403366295998
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 15:05:07 -0500

    declare variable in CRACustomer class

--- app/src/main/java/com/example/taxcalculator/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculator/CRACustomer.java
@@ -1,4 +1,13 @@
 package com.example.taxcalculator;
 
 public class CRACustomer {
+    Integer sinNumber;
+    Integer age;
+    String fName,lName;
+    String fullName;
+    String gender;
+    String dateOfBirth;
+    String taxFillingDate;
+    double grossIncome, federalTax, provicialTax, empInsurance;
+    double rrspContri, rrspCarryForward, taxableIncome, taxPaid;
 }
