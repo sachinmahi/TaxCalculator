551ab3722771300d3b9ccf1277b0e7c7765e21a2
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 15:10:43 -0500

    changes in TaxDetails

--- app/src/main/java/com/example/taxcalculator/TaxDetails.java
+++ app/src/main/java/com/example/taxcalculator/TaxDetails.java
@@ -34,5 +34,24 @@ public class TaxDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tax_details);
+
+        calculatedfullName = findViewById(R.id.txtCalFullName);
+        calSIN =findViewById(R.id.edCalSinNumber);
+        calAge = findViewById(R.id.txtCalAge);
+        calGender = findViewById(R.id.calRadioGender);
+        calDOB = findViewById(R.id.txtCalDOB);
+        calTaxFillingDate = findViewById(R.id.txtCalFillingDate);
+
+        txtDgrossIncome = findViewById(R.id.txtCalGrossIncome);
+        txtDfederalTax = findViewById(R.id.txtCalFederalTax);
+        txtDprovincialTax = findViewById(R.id.txtCalProvincialTax);
+        txtCPP = findViewById(R.id.txtCalCPP);
+        txtEmpInsurance= findViewById(R.id.txtCalEI);
+        txtRRSPContribution = findViewById(R.id.txtCalRRSPContributed);
+        txtRRSPCarryForward = findViewById(R.id.txtCalCarryForwardRRSP);
+        txtTaxableIncome = findViewById(R.id.txtCalTotalTaxableIncome);
+        txtTaxPaid = findViewById(R.id.txtCalTotalTaxPaid);
+
+        
     }
 }
