204559c939c034f2f01761803a3d06684fea666f
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 17:22:42 -0500

    update

--- app/src/main/java/com/example/taxcalculator/TaxDetails.java
+++ app/src/main/java/com/example/taxcalculator/TaxDetails.java
@@ -152,37 +152,67 @@ public class TaxDetails extends AppCompatActivity {
         }
         return cpp;
     }
-    public double calcFedralTax() {
+    public double calcFedralTax(){
         //calculate federal tax
-        double temp = taxableIncome;
-        if (taxableIncome < 12069.00) {
-            federalTax = 0;
+        double temp = taxableIncome ;
+        if(temp <= 12069.00){
+            federalTax = 0;//0%
             temp = taxableIncome - 12069.00;
-        } else if (temp < 47630.00) {
-            federalTax = (temp * 0.15);
-            temp = temp - federalTax;
-        } else if (temp < 95259.00) {
+        }
+        if(temp >= 12069.01){
+            federalTax = (temp * 0.15);//15%
+            temp = temp - 35561;
+        }
+        if(temp >= 47630.01){
             federalTax = (temp * 0.205); //20.50%
-            temp = temp - federalTax;
-        } else if (temp < 147667.00) {
+            temp = temp - 47628.99;
+        }
+        if(temp >= 95259.01){
             federalTax = (temp * 0.26); //26%
-            temp = temp - federalTax;
-        } else if (temp < 210371.00) {
+            temp = temp - 52407.99;
+        }
+        if (temp >= 147667.01){
             federalTax = (temp * 0.29);//29%
-            temp = temp - federalTax;
-        } else {
+            temp = temp - 62703.99;
+        }
+        if(temp >= 210371.01){
             federalTax = (temp * 0.33);//33%
-            temp = temp - federalTax;
+            //temp = temp - federalTax;
         }
         return federalTax;
     }
     public  double calcProvincialTax(){
         //calculate provincial tax
+        double temp = taxableIncome ;
+
+        if(temp <= 10582.00){
+            provincialTax = 0;
+            temp = taxableIncome - 10582.00;
+        }
+        if(temp >= 10582.01){
+            provincialTax = (temp * 0.0505); //5.05%
+            temp = temp - 33323.99;
+        }
+        if(temp >= 43906.01){
+            provincialTax = (temp * 0.0915); //9.15%
+            temp = temp - 43906.99;
+        }
+        if(temp >= 87813.01){
+            provincialTax = (temp * 0.1116); //11.16%
+            temp = temp - 62187.99;
+        }
+        if (temp >= 150000.01){
+            provincialTax = (temp * 0.1216);//12.16%
+            temp = temp - 69999.99;
+        }
+        if(temp >= 220000.01){
+            provincialTax = (temp * 0.1316);//13.16%
+
+        }
         return provincialTax;
     }
-    public  double calTaxPaid()
-    {
-        return totalTaxPaid;
+    public  double calTaxPaid(){
+        return totalTaxPaid = federalTax + provincialTax;
     }
 
 }
