8b9905fd1a2e5bd12a8beb530516b27c0d67486f
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 15:03:40 -0500

    declare variable in TaxDetails Activity

--- app/src/main/java/com/example/taxcalculator/TaxDetails.java
+++ app/src/main/java/com/example/taxcalculator/TaxDetails.java
@@ -3,8 +3,27 @@ package com.example.taxcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaxDetails extends AppCompatActivity {
+
+    private TextView calculatedfullName;
+    private TextView calSIN;
+    private TextView calAge;
+    private TextView calGender;
+    private TextView calDOB;
+    private TextView calTaxFillingDate;
+
+    private TextView  txtDgrossIncome;
+    private TextView txtDfederalTax;
+    private TextView txtDprovincialTax;
+    private TextView txtCPP;
+    private TextView txtEmpInsurance;
+    private TextView txtRRSPContribution;
+    private TextView txtRRSPCarryForward;
+    private TextView txtTaxableIncome;
+    private TextView txtTaxPaid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
