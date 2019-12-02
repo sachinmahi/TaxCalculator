eb10c2455b7172193719a7bfbb75046f2605e6a1
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 15:06:49 -0500

    get set method in CRACustomer class

--- app/src/main/java/com/example/taxcalculator/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculator/CRACustomer.java
@@ -39,4 +39,73 @@ public class CRACustomer {
         this.grossIncome = grossIncome;
         this.rrspContri = rrspContri;
     }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getFederalTax() {
+        return federalTax;
+    }
+
+    public void setFederalTax(double federalTax) {
+        this.federalTax = federalTax;
+    }
+
+    public double getProvicialTax() {
+        return provicialTax;
+    }
+
+    public void setProvicialTax(double provicialTax) {
+        this.provicialTax = provicialTax;
+    }
+
+    public double getEmpInsurance() {
+        return empInsurance;
+    }
+
+    public void setEmpInsurance(double empInsurance) {
+        this.empInsurance = empInsurance;
+    }
+
+    public double getRrspContri() {
+        return rrspContri;
+    }
+
+    public void setRrspContri(double rrspContri) {
+        this.rrspContri = rrspContri;
+    }
+
+    public double getRrspCarryForward() {
+        return rrspCarryForward;
+    }
+
+    public void setRrspCarryForward(double rrspCarryForward) {
+        this.rrspCarryForward = rrspCarryForward;
+    }
+
+    public double getTaxableIncome() {
+        return taxableIncome;
+    }
+
+    public void setTaxableIncome(double taxableIncome) {
+        this.taxableIncome = taxableIncome;
+    }
+
+    public double getTaxPaid() {
+        return taxPaid;
+    }
+
+    public void setTaxPaid(double taxPaid) {
+        this.taxPaid = taxPaid;
+    }
+
+    //    public CRACustomer(String fullName)
+//    {
+//       this.fullName = fullName;
+//    }
 }
